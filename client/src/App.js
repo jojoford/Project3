7be10f95494d79f80dd25daf7c6c8e01bfe44b9c
symlara <40181569+symlara@users.noreply.github.com>
2021-05-18 17:21:55 -0500

    updating App.js file with Nav info. added auth file to utils folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloClient } from 'apollo-boost';
 
 
 import About from './components/About';
+import Nav from './components/Nav';
+
+
 function App() {
 
   return (
+    <ApolloProvider>
+      <Router>
     <div>
+      <Nav />
       <main>
         <About></About>
       </main>
     </div>
+    </Router>
+   </ApolloProvider>
   );
 }
 
