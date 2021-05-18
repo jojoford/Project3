8008a926a9e51676f8d13597ca03ad8306cc5e54
symlara <40181569+symlara@users.noreply.github.com>
2021-05-18 18:55:09 -0500

    added apolloclient and nav code to app.js file. added userschema to models and user info into schemas folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
-import { ApolloClient } from 'apollo-boost';
+import  ApolloClient from 'apollo-boost';
 
-
+// add detail page for photographers?
+// add home page? This will have seeded data of their work?
+// about page would be about app itself
 import About from './components/About';
 import Nav from './components/Nav';
 
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
+
 
 function App() {
-
-  return (
-    <ApolloProvider>
+    return (
+    <ApolloProvider client={client}>
       <Router>
     <div>
       <Nav />
-      <main>
+      <Switch>
+        <Route exact path="/about" component={About} />
+      </Switch>
         <About></About>
-      </main>
     </div>
     </Router>
    </ApolloProvider>
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,8 +4,51 @@ import { Link } from 'react-router-dom';
 
 function Nav() {
     function showNavigation() {
-        if (Auth.loggedIn()) {
-            
+        // add if/else statement to this once auth page in utils folder is done
+            return (
+                <ul>
+                    <li>
+                        <Link to="/about">
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <a href="/" onClick={() => Auth.logout()}>
+                            Logout
+                        </a>
+                    </li>
+                </ul>
+            );
+       // this is for the auth functinality
+            // return (
+            //     <ul>
+            //         <li>
+            //             <Link to="/signup">
+            //                 Signup
+            //             </Link>
+            //         </li>
+            //         <li>
+            //             <Link to="/login">
+            //                 Login
+            //             </Link>
+            //         </li>
+            //     </ul>
+            // );
         }
+        return (
+            <header>
+                <h1>
+                <Link to="/"></Link>
+                </h1>
+    
+                <nav>
+                    {showNavigation()}
+                </nav>
+            </header>
+        );
     }
-}
+
+   
+
+
+export default Nav;
