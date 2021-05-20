47980fbe8adcbf0420d114280ccebd49402e2332
jojoford <75343703+jojoford@users.noreply.github.com>
2021-05-19 20:24:41 -0500

    new folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
-import { ApolloClient } from 'apollo-boost';
+import  ApolloClient  from 'apollo-boost';
 
 import Nav from './components/Nav';
 import About from './components/About';
 import Carousel from './components/Carousel';
 
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql',
+})
 
 function App() {
-    return (
-    <ApolloProvider client={client}>
-      <Router>
-    <div>
-      <Nav></Nav>
-      <main>
-        <Carousel></Carousel>
-        {/* <About></About> */}
-        </main>
-    </div>
-    </Router>
-   </ApolloProvider>
-  );
+  return (
+  <ApolloProvider client={client}>
+    <Router>
+  <div>
+    <Nav />
+    <Switch>
+      <Route exact path="/about" component={About} />
+    </Switch>
+      <Carousel></Carousel>
+      {/* <About></About> */}
+  </div>
+  </Router>
+ </ApolloProvider>
+);
 }
 export default App; 
