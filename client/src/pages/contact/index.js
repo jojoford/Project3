27122aa40f5f75ffb735b0a0f5c692f7e78b946e
symlara <40181569+symlara@users.noreply.github.com>
2021-05-25 14:46:35 -0500

    re-installed mongoose and mongodb. added seeded user data

--- client/src/pages/contact/index.js
+++ client/src/pages/contact/index.js
@@ -14,13 +14,14 @@ function Contact() {
         
         <div className="contact-container">
           <div className="smaller-container">
-            <src url="https://docs.google.com/forms/d/e/1FAIpQLSd2CJ2zsYwSF6ndw23-v6rt_ZNfU3GneBPI1J-lA8RgZgscNw/viewform?embedded=true"
+     
+             {/* <src url="https://docs.google.com/forms/d/e/1FAIpQLSd2CJ2zsYwSF6ndw23-v6rt_ZNfU3GneBPI1J-lA8RgZgscNw/viewform?embedded=true" 
               width="1500px"
               height="753px"
               id="forms"
               className="myClassname"
               display="initial"
-              position="relative" />
+              position="relative" /> */}
           </div>
         </div>
       </div>
--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,2 +1,17 @@
 // place data below that that will appear on site before any user input
 
+const db = require('./connection');
+const { User } = require('../models');
+
+db.once('open', async () => {
+    await User.create({
+        firstName: 'Tester',
+        lastName: 'Test',
+        email: "tester@email.com",
+        password: "password123"
+    });
+
+    console.log('users seeded');
+
+    process.exit();
+});
