c6ff01578fc6321e7cee26621e3168cc22cb5d14
symlara <40181569+symlara@users.noreply.github.com>
2021-05-30 17:21:53 -0500

    added reducers file

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Carousel from '../components/Carousel';
 // import Nav from '../components/Nav';
+// import Cart from '../components/Cart';
 
 const Home = () => {
     return (
         <div className="carousel">
+            {/* <Cart /> */}
             {/* <Nav></Nav>  */}
             <Carousel></Carousel>
     
