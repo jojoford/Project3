cf8acd7271d23863cfcd56da0223f48f960e8eb4
symlara <40181569+symlara@users.noreply.github.com>
2021-05-30 21:56:42 -0500

    removed comments

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
     return (
         <footer className="footer">
             <p style={{backgroundColor: "black", color: 'white'}}>©PhotoAlbum</p>
-            <p style={{backgroundColor: "black", color: 'white'}}><span>Creating with ❤️ by <a href="https://github.com/jojoford/" target="_blank" rel="noopener noreferrer">jojoford</a> | <a href="https://github.com/symlara"  target="_blank" rel="noopener noreferrer">symlara</a></span></p>
+            <p style={{backgroundColor: "black", color: 'white'}}><span>Creating with ❤️ by <a href="https://github.com/jojoford/" target="_blank" rel="noopener noreferrer">jojoford</a> | <a href="https://github.com/symlara"  target="_blank" rel="noopener noreferrer">symlara</a> | <a href="/" target="_blank" rel="noopener noreferrer">Ivy</a></span></p>
         </footer>
     )
 };
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,22 +4,9 @@ import { Link } from "react-router-dom";
 // import LoginForm from './LoginForm';
 // import Auth from '../../utils/auth';
 import './nav.css';
-//alyse added link 
+
 function Nav() {
     
-
-//   const  categories = [
-//     { name: 'blackAndWhite', description: 'Photos in black and white' },
-//     { name: 'portraits', description: 'Portraits of people in my life' },
-//     { name: 'landscape', description: 'The beauty of nature' }
-//   ];
-
-  // const {
-  //   // setCurrentCategory,
-  //   // currentCategory,
-  //   // contactSelected,
-  //   // setContactSelected
-  // } = props;
 
       return (
           <div>
@@ -57,96 +44,4 @@ function Nav() {
                 
             
       
-    //     <div className="topnav" id="Topnav">
-    //       {/* <h1>
-    //         <Link to="/" className="active">Photo-Album📸</Link>
-    //         </h1> */}
-        
-    //   <nav>
-    //       <div className="router-link">
-    //       <Link className="text-decor" to="/about">About</Link>
-    //                 </div>
-    //       <div className="router-link">
-    //         <Link className="text-decor" to="/artists">Artists</Link>
-    //                 </div>
-    //       <div className="router-link">
-    //         <Link className="text-decor" to="/contact">Contact</Link>
-    //                 </div>
-    
-    //         <div className="router-link">
-            
-    //       <Link to="/portraits">Portraits</Link>
-    //                   </div>
-    
-    //         <div className="router-link">
-    //      <Link to="/blackandwhites">Black and Whites</Link>
-    //                   </div>
-    
-    //         <div className="router-link">
-    //         <Link to="/landscapes">Landscapes</Link>
-    //                   </div>   
-    //             </nav>
-                
-    // </div>
-    
-      
-                 
   
-  
- 
-
-        
-
-
-      
-        
-
-   
-
-
-/*import React from 'react';
-import { Link } from "react-router-dom";
-import './Nav.css';
-import Login from "../Login"
-import Logout from "../Logout"
-export default class Navbar extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div className='navbtn-grid' >
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation" ><i className="fas fa-bars fa-2x nav-btn"></i></button>
-                </div>
-                <div className='nav-grid collapse' id='nav-content'>
-                    <div>
-                        <div className="router-link">
-                            <Link className="text-decor" to="/">home</Link>
-                        </div>
-                        <div className="router-link">
-                            <Link className="text-decor" to="/photos">photos</Link>
-                        </div>
-                        <div className="router-link">
-                            <Link className="text-decor" to="/artists">artists</Link>
-                        </div>
-                        <div className="router-link">
-                            <Link className="text-decor" to="/shopcart">shop</Link>
-                        </div>
-                        <div className="router-link">
-                            <Link className="text-decor" to="/about">about</Link>
-                        </div>
-                        <div className="router-link">
-                            <Link className="text-decor" to="/contact">contact</Link>
-                        </div>
-                        <div className="router-link">
-                            <Login></Login>
-                        </div>
-                        <div className="router-link">
-                            <Logout></Logout>
-                        </div>
-                        
-                    </div>
-                </div>
-            </React.Fragment>
-        )
-    }
-}
-*/
